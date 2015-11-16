6dacc2d3fb3e2b5e311f69b8d57050fe33562c45
katokaisya <katokaisya@email.com>
2015-11-16 05:00:48 +0000

    カートの追加

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,11 @@ class StoreController < ApplicationController
 
   def index
     @products=Product.order(:title).first(5)
+
+    puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
+    puts session[:counter]
+    puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
+    (session[:counter].nil?) ? session[:counter]=1 : session[:counter]+=1
+    @counter="#{session[:counter]}回目のアクセスです。"
   end
 end

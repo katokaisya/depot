83a6b7ad3d59800dc27bb7e32e739dc59402dcbf
katokaisya <katokaisya@email.com>
2015-11-24 00:36:20 +0000

    ログイン系の処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     before_action :now, except: [:show, :edit, :update, :destroy]
+    before_action :authorize
+    before_action :basic  #ベーシック認証テスト
+
   def now
     @time=Time.zone.now.strftime("%Y年 %m月 %d日, %H:%M:%S")
   end
 
   private
+#ベーシック認証テスト~ここから(htmlリクエストではない場合)
+  def basic
+    puts "==============#{request.format}========"
+    unless request.format == 'text/html'
+      authenticate_or_request_with_http_basic do |user, pass|
+        user == 'user' && pass == 'pass'
+      end
+    end
+  end
+#ベーシック認証テスト~ここまで
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url,notice: "ログインしてください"
+    end
+  end
+
   def current_cart
     Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,11 @@
 class StoreController < ApplicationController
-
+  skip_before_action :authorize
   def index
     @products=Product.order(:title).first(5)
 
-    puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
-    puts session[:counter]
-    puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
+    #puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
+    #puts session[:counter]
+    #puts "デバッグ用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝"
     (session[:counter].nil?) ? session[:counter]=1 : session[:counter]+=1
     @counter="#{session[:counter]}回目のアクセスです。"
     @cart=current_cart    #@cartを定義
